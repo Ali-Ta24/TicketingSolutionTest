0cdde04d9fd747e802e1a07123e9188f79eea307
AliT <a.ali.talebi@gmail.com>
2023-02-02 18:59:03 +0330

    add SeviceBookingResult By Test

---
+++ TicketingSolution.Core.Test/TicketBookingRequestHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace TicketingSolution.Core
+{
+    public class TicketBookingRequestHandler
+    {
+        public TicketBookingRequestHandler()
+        {
+        }
+
+        public ServiceBookingResult BookServece(TicketBookingRequest bookingRequest)
+        {
+            var result = new ServiceBookingResult()
+            {
+                Name = bookingRequest.Name,
+                Family = bookingRequest.Family,
+                Email = bookingRequest.Email
+            };
+
+            return result;
+        }
+    }
+}
---
+++ TicketingSolution.Core.Test/Ticket_Booking_Requet_Handler_Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TicketingSolution.Core
+{
+    public class Ticket_Booking_Requet_Handler_Test
+    {
+        [Fact]
+        public void Should_Return_Ticket_Booking_Response_With_Request_Values()
+        {
+            var BookingRequest = new TicketBookingRequest
+            {
+                Name = "Test Name",
+                Family = "Test Family",
+                Email = "TestEmail"
+            };
+
+            var Handler = new TicketBookingRequestHandler();
+
+            ServiceBookingResult Result = Handler.BookServece(BookingRequest);
+
+            Assert.NotNull(Result);
+            Assert.Equal(BookingRequest.Name, Result.Name);
+            Assert.Equal(BookingRequest.Family, Result.Family);
+            Assert.Equal(BookingRequest.Email, Result.Email);
+        }
+    }
+}
